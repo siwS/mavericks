ba4c9b4e9d273ef7a699da55448c8876e9e129c9
sofia <tzi.sof@gmail.com>
2019-11-17 11:12:48 +0000

    Filter out suspended

--- tweets_scrapper/tweets_scrapper.rb
+++ tweets_scrapper/tweets_scrapper.rb
@@ -1,30 +1,29 @@
 require 'httparty'
 require 'nokogiri'
 
-file = "benevolent_sexist.tsv"
-benevolent_sexist = []
+file = "hostile_sexist.tsv"
+tweet_ids = []
 
 File.open(file, "r") do |f|
   f.each_line do |line|
-    benevolent_sexist << line
+    tweet_ids << line
   end
 end
 
-output_file = File.open("output", "w")
+output_file = File.open("output_hostile_100", "w")
 
 tweets = []
 
 
-benevolent_sexist.each do |tweet|
+tweet_ids.first(100).each do |tweet|
 	url = "https://twitter.com/_/status/"+tweet.strip
 	puts url
 	response = HTTParty.get(url)
 	parsed_response = response.parsed_response
 	parsed_data = Nokogiri::HTML.parse(parsed_response)
 	title = parsed_data.title
-	tweets << title
- 	
+	tweets << title unless title.include?("Account Suspended")
 end
 
-file.write(tweets)
-file.close
+output_file.write(tweets)
+output_file.close
