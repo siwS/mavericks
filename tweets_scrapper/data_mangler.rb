aaf11d48d9bf5fe1977caae887d0aa8f8b0003ee
Serena Fritsch <serena@intercom.io>
2019-11-24 18:13:35 +0000

    new cleaned up data files

---
+++ tweets_scrapper/data_mangler.rb
@@ -0,0 +1,24 @@
+require 'csv'
+require 'pry'
+
+file = "output_hostile.csv"
+output_file = "new_output_hostile.csv"
+
+results = []
+rows = CSV.read(file, liberal_parsing: true)
+rows.each_with_index do |row, index|
+  puts index
+  result = row[0].split(":")
+  results << result[1]
+end
+
+
+puts results
+CSV.open(output_file, 'w') do |csv_object|
+  results.each do |row_array|
+    puts row_array
+    csv_object << [row_array]
+  end
+end
+
+
